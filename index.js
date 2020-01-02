cba3f2155a93d31897b6b736fb7e91c0acf67c75
paisleypark <paisleypark@outlook.it>
2020-01-02 22:02:34 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.listen(3000,() => {
+    console.log('listening');
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
