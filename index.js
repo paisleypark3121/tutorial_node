6c99b3c0de417681c53efc8b582d8670e89b878a
paisleypark <paisleypark@outlook.it>
2020-01-03 10:55:51 +0100

    process.env.PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.use(bodyParser.json());
 
-app.listen(3000,() => {
+app.listen(process.env.PORT || 3000,() => {
     console.log('listening');
 });
 
